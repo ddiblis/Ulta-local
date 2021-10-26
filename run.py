cf1ff29905d248cbd47e9ac49ce4dafb6c26d129
“sdl <sdltesterusa131@gmail.com>
2021-10-25 21:53:50 -0600

    fix(Executed-code): done

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-from ulta.main import get_details
+from ulta.main import get_details, save_json
 
 if __name__ == '__main__':
-  get_details()
   # save_json()
+  get_details()
 # print(get_cats())
